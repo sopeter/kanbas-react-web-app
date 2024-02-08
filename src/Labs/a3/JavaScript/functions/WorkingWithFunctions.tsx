7e2f9d2244a8cf6f45031446fb86aa58fa2996e0
Peter So <peterjso23@gmail.com>
2024-02-07 22:58:55 -0500

    a3 lab done

---
+++ src/Labs/a3/JavaScript/functions/WorkingWithFunctions.tsx
@@ -0,0 +1,17 @@
+import ArrowFunctions from "./ArrowFunctions";
+import ES5Functions from "./ES5Functions";
+import FunctionParanthesisAndParameters from "./FunctionParanthesisAndParameters";
+import ImpliedReturn from "./ImpliedReturn";
+
+function WorkingWithFunctions() {
+  return (
+    <div>
+      <ES5Functions/>
+      <ArrowFunctions/>
+      <ImpliedReturn/>
+      <FunctionParanthesisAndParameters/>
+    </div>
+  );
+}
+
+export default WorkingWithFunctions;
