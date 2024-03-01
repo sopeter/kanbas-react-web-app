17c87bb7759e58676cfb1f1faffda52496328fbc
Peter So <peterjso23@gmail.com>
2024-03-01 12:23:54 -0500

    lab a4 complete

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -11,6 +11,7 @@ import ObjectStateVariable from "./ObjectStateVariable";
 import ArrayStateVariable from "./ArrayStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
 import ReduxExamples from "./ReduxExamples";
+import TodoList from "./ReduxExamples/todos/TodoList";
 
 const Assignment4 = () => {
   function sayHello() {
@@ -31,6 +32,7 @@ const Assignment4 = () => {
       <ObjectStateVariable/>
       <ArrayStateVariable />
       <ParentStateComponent />
+      <TodoList/>
     </>
   );
 };
