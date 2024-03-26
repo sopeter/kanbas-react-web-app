a8a2caeb41665c0b9615d8f4c7d7d3699c1e2478
Peter So <peterjso23@gmail.com>
2024-03-26 16:54:59 -0400

    open all modules

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -173,7 +173,9 @@ function ModuleList() {
                   <FaEllipsisV className="ms-2" />
                 </span>
               </div>
-              {selectedModule._id === module._id && (
+              {
+                // selectedModule._id === module._id && 
+              (
                 <ul className="list-group">
                   {module.lessons?.map((lesson: any) => (
                     <li className="list-group-item">
